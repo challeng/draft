65cd4d9db1ac9e9fc4444771533b2f11d7f64f21
Jim Challenger <jim.challenger1@gmail.com>
2013-12-21 01:17:00 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DotaDraft::Application.config.secret_key_base = '7acd213bf0b35c573828d11e15241f213498983bb991a74f16a9c21c235b161c575ed7c9652f646653dfb7b55221211bd99d35b470b7df185169189c9daa9f77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DotaDraft::Application.config.session_store :cookie_store, key: '_dota_draft_session'
