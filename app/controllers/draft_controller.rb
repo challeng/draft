a3ec9b0ef252b4551a181fd3e6a87124affcd01b
Jim Challenger <jim.challenger1@gmail.com>
2014-01-21 23:40:31 -0600

    adding intial work for draft and using slim

---
+++ app/controllers/draft_controller.rb
@@ -0,0 +1,5 @@
+class DraftController < ApplicationController
+	def index
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 DotaDraft::Application.routes.draw do
+  root 'draft#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
